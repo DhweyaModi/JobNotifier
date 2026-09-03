f4b18e56466cb50fa20118fa57817e82585c2c0f
Dhweya Modi <dhweya.modi@gmail.com>
2026-09-02 20:02:39 -0700

    Fixing next.js error.

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -31,9 +31,13 @@ export default function RootLayout({
   return (
     <html
       lang="en"
+      suppressHydrationWarning
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col bg-[#0b0f19] text-slate-100">
+      <body
+        suppressHydrationWarning
+        className="min-h-full flex flex-col bg-[#0b0f19] text-slate-100"
+      >
         <AuthProvider>
           {children}
         </AuthProvider>
